12a6f811df0a6931f9799309a5c2f57965f88613
bruno173 <bruno@ascalia.io>
2025-03-10 08:15:43 +0100

    add more tests for working maps, add non working maps, add helpers and finish findPath function for working maps only

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,48 +6,66 @@ describe('findPath', () => {
   it('should find the correct path for BASIC_EXAMPLE', () => {
     const map2D = handleParseMap(MAPS.BASIC_EXAMPLE);
     const pathString = '@---A---+|C|+---+|+-B-x';
+    const collectedLetters = 'ACB';
     const expectedPath = pathString.split('');
+    const expectedLetters = collectedLetters.split('');
     const result = findPath(map2D);
-    expect(result).toEqual(expectedPath);
+    expect(result.path).toEqual(expectedPath);
+    expect(result.collectedLetters).toEqual(expectedLetters);
   });
 
   it('should find the correct path for STRAIGHT_THROUGH_INTERSECTIONS', () => {
     const map2D = handleParseMap(MAPS.STRAIGHT_THROUGH_INTERSECTIONS);
     const pathString = '@|A+---B--+|+--C-+|-||+---D--+|x';
+    const collectedLetters = 'ABCD';
     const expectedPath = pathString.split('');
+    const expectedLetters = collectedLetters.split('');
     const result = findPath(map2D);
-    expect(result).toEqual(expectedPath);
+    expect(result.path).toEqual(expectedPath);
+    expect(result.collectedLetters).toEqual(expectedLetters);
   });
 
   it('should find the correct path for LETTERS_ON_TURNS', () => {
     const map2D = handleParseMap(MAPS.LETTERS_ON_TURNS);
     const pathString = '@---A---+|||C---+|+-B-x';
+    const collectedLetters = 'ACB';
     const expectedPath = pathString.split('');
+    const expectedLetters = collectedLetters.split('');
     const result = findPath(map2D);
-    expect(result).toEqual(expectedPath);
+    expect(result.path).toEqual(expectedPath);
+    expect(result.collectedLetters).toEqual(expectedLetters);
   });
 
   it('should find the correct path for NO_DUPLICATE_LETTERS', () => {
     const map2D = handleParseMap(MAPS.NO_DUPLICATE_LETTERS);
     const pathString = '@-G-O-+|+-+|O||+-O-N-+|I|+-+|+-I-+|ES|x';
+    const collectedLetters = 'GOONIES';
     const expectedPath = pathString.split('');
+    const expectedLetters = collectedLetters.split('');
     const result = findPath(map2D);
-    expect(result).toEqual(expectedPath);
+    expect(result.path).toEqual(expectedPath);
+    expect(result.collectedLetters).toEqual(expectedLetters);
   });
 
   it('should find the correct path for COMPACT_SPACE', () => {
     const map2D = handleParseMap(MAPS.COMPACT_SPACE);
     const pathString = '@B+++B|+-L-+A+++A-+Hx';
+    const collectedLetters = 'BLAH';
     const expectedPath = pathString.split('');
+    const expectedLetters = collectedLetters.split('');
     const result = findPath(map2D);
-    expect(result).toEqual(expectedPath);
+    expect(result.path).toEqual(expectedPath);
+    expect(result.collectedLetters).toEqual(expectedLetters);
   });
 
   it('should find the correct path for IGNORE_AFTER_END', () => {
     const map2D = handleParseMap(MAPS.IGNORE_AFTER_END);
     const pathString = '@-A--+|+-B--x';
+    const collectedLetters = 'AB';
     const expectedPath = pathString.split('');
+    const expectedLetters = collectedLetters.split('');
     const result = findPath(map2D);
-    expect(result).toEqual(expectedPath);
+    expect(result.path).toEqual(expectedPath);
+    expect(result.collectedLetters).toEqual(expectedLetters);
   });
 });
--- src/helpers/pathFinder.ts
+++ src/helpers/pathFinder.ts
@@ -1,126 +1,208 @@
-import { getStartingAndEndingCharacterPosition, nextPositionBasedOnPreviousDirection } from 'src/helpers/positionGetters';
+import { getStartingAndEndingCharacterPosition } from './positionGetters';
 
-type MapArray = string[][];
+interface PathResult {
+  path: string[];
+  collectedLetters: string[];
+  error: string | null;
+}
+type Position = { rowIndex: number; cellIndex: number };
 type Directions = 'up' | 'down' | 'left' | 'right';
+type MapArray = string[][];
 
-export const findPath = (map2D: MapArray) => {
-  const { startingPosition, endingPosition } = getStartingAndEndingCharacterPosition(map2D);
+export const findPath = (map2D: MapArray): PathResult => {
+  const { startingPosition, endingPosition, errorMessage } = getStartingAndEndingCharacterPosition(map2D);
+  if (errorMessage) return { path: [], collectedLetters: [], error: errorMessage };
+
   const position = { ...startingPosition };
   const path = [];
+  const collectedLetters = [];
+  const visited = new Set<string>();
   let previousDirection: Directions | null = null;
 
   // Determine the starting direction
   if (map2D[position.rowIndex][position.cellIndex] === '@') {
     path.push('@');
-    if (position.cellIndex + 1 < map2D[0].length && (map2D[position.rowIndex][position.cellIndex + 1] === '-' || /[A-Z]/.test(map2D[position.rowIndex][position.cellIndex + 1]) || map2D[position.rowIndex][position.cellIndex + 1] === '+')) {
-      position.cellIndex++;
-      previousDirection = 'right';
-    } else if (position.cellIndex - 1 >= 0 && (map2D[position.rowIndex][position.cellIndex - 1] === '-' || /[A-Z]/.test(map2D[position.rowIndex][position.cellIndex - 1]) || map2D[position.rowIndex][position.cellIndex - 1] === '+')) {
-      position.cellIndex--;
-      previousDirection = 'left';
-    } else if (position.rowIndex + 1 < map2D.length && (map2D[position.rowIndex + 1][position.cellIndex] === '|' || /[A-Z]/.test(map2D[position.rowIndex + 1][position.cellIndex]) || map2D[position.rowIndex + 1][position.cellIndex] === '+')) {
-      position.rowIndex++;
-      previousDirection = 'down';
-    } else if (position.rowIndex - 1 >= 0 && (map2D[position.rowIndex - 1][position.cellIndex] === '|' || /[A-Z]/.test(map2D[position.rowIndex - 1][position.cellIndex]) || map2D[position.rowIndex - 1][position.cellIndex] === '+')) {
-      position.rowIndex--;
-      previousDirection = 'up';
-    }
+    previousDirection = determineStartingDirection(map2D, position);
+    if (!previousDirection) return { path: [], collectedLetters: [], error: 'Error: No valid starting direction' };
   }
 
   // eslint-disable-next-line no-constant-condition
   while (true) {
-    if (position.rowIndex < 0 || position.rowIndex >= map2D.length || position.cellIndex < 0 || position.cellIndex >= map2D[0].length) {
-      break;
-    }
+    if (isOutOfBounds(map2D, position)) break;
 
-    const currentChar = map2D[position.rowIndex][position.cellIndex];
+    const posKey = `${position.rowIndex},${position.cellIndex}`;
+    const currentChar = map2D[position.rowIndex]?.[position.cellIndex];
 
-    if (currentChar === '-') {
-      path.push('-');
-      if (previousDirection) {
-        nextPositionBasedOnPreviousDirection(position, previousDirection);
-      } else {
-        break;
-      }
-    } else if (currentChar === '|') {
-      path.push('|');
-      if (previousDirection) {
-        nextPositionBasedOnPreviousDirection(position, previousDirection);
-      } else {
-        break;
-      }
-    } else if (/[A-Z]/.test(currentChar)) {
+    if (currentChar === 'x') {
       path.push(currentChar);
-      // Try to continue in the previous direction first
-      if (previousDirection === 'right' && position.cellIndex + 1 < map2D[0].length && map2D[position.rowIndex][position.cellIndex + 1] !== ' ') {
-        position.cellIndex++;
-      } else if (previousDirection === 'left' && position.cellIndex - 1 >= 0 && map2D[position.rowIndex][position.cellIndex - 1] !== ' ') {
-        position.cellIndex--;
-      } else if (previousDirection === 'down' && position.rowIndex + 1 < map2D.length && map2D[position.rowIndex + 1][position.cellIndex] !== ' ') {
-        position.rowIndex++;
-      } else if (previousDirection === 'up' && position.rowIndex - 1 >= 0 && map2D[position.rowIndex - 1][position.cellIndex] !== ' ') {
-        position.rowIndex--;
-      } else {
-        // Check all possible directions except the one we came from
-        if (previousDirection !== 'left' && position.cellIndex + 1 < map2D[0].length && map2D[position.rowIndex][position.cellIndex + 1] !== ' ') {
-          position.cellIndex++;
-          previousDirection = 'right';
-        } else if (previousDirection !== 'right' && position.cellIndex - 1 >= 0 && map2D[position.rowIndex][position.cellIndex - 1] !== ' ') {
-          position.cellIndex--;
-          previousDirection = 'left';
-        } else if (previousDirection !== 'up' && position.rowIndex + 1 < map2D.length && map2D[position.rowIndex + 1][position.cellIndex] !== ' ') {
-          position.rowIndex++;
-          previousDirection = 'down';
-        } else if (previousDirection !== 'down' && position.rowIndex - 1 >= 0 && map2D[position.rowIndex - 1][position.cellIndex] !== ' ') {
-          position.rowIndex--;
-          previousDirection = 'up';
-        } else {
-          break;
-        }
-      }
-    } else if (currentChar === '+') {
-      path.push('+');
-      // Try to continue in the previous direction first
-      if (previousDirection === 'right' && position.cellIndex + 1 < map2D[0].length && map2D[position.rowIndex][position.cellIndex + 1] !== ' ') {
-        position.cellIndex++;
-      } else if (previousDirection === 'left' && position.cellIndex - 1 >= 0 && map2D[position.rowIndex][position.cellIndex - 1] !== ' ') {
-        position.cellIndex--;
-      } else if (previousDirection === 'down' && position.rowIndex + 1 < map2D.length && map2D[position.rowIndex + 1][position.cellIndex] !== ' ') {
-        position.rowIndex++;
-      } else if (previousDirection === 'up' && position.rowIndex - 1 >= 0 && map2D[position.rowIndex - 1][position.cellIndex] !== ' ') {
-        position.rowIndex--;
-      } else {
-        // Check all possible directions except the one we came from
-        if (previousDirection !== 'left' && position.cellIndex + 1 < map2D[0].length && map2D[position.rowIndex][position.cellIndex + 1] !== ' ') {
-          position.cellIndex++;
-          previousDirection = 'right';
-        } else if (previousDirection !== 'right' && position.cellIndex - 1 >= 0 && map2D[position.rowIndex][position.cellIndex - 1] !== ' ') {
-          position.cellIndex--;
-          previousDirection = 'left';
-        } else if (previousDirection !== 'up' && position.rowIndex + 1 < map2D.length && map2D[position.rowIndex + 1][position.cellIndex] !== ' ') {
-          position.rowIndex++;
-          previousDirection = 'down';
-        } else if (previousDirection !== 'down' && position.rowIndex - 1 >= 0 && map2D[position.rowIndex - 1][position.cellIndex] !== ' ') {
-          position.rowIndex--;
-          previousDirection = 'up';
-        } else {
-          break;
-        }
-      }
-    } else if (currentChar === 'x') {
-      path.push('x');
-      break;
-    } else {
       break;
     }
 
-    if (position.rowIndex === endingPosition.rowIndex && position.cellIndex === endingPosition.cellIndex) {
-      path.push('x');
-      break;
+    if (visited.has(posKey)) {
+      path.push(currentChar);
+      const possibleDirections: Directions[] = [previousDirection, 'right', 'left', 'down', 'up'].filter((dir): dir is Directions => dir !== null);
+      let foundUnvisited = false;
+      for (const direction of possibleDirections) {
+        const newPosition = { ...position };
+        moveInDirection(newPosition, direction);
+        if (isOutOfBounds(map2D, newPosition)) continue;
+        const newPosKey = `${newPosition.rowIndex},${newPosition.cellIndex}`;
+
+        if (!visited.has(newPosKey) && isValidMove(map2D, position, direction)) {
+          if (/[A-Z]/.test(map2D[newPosition.rowIndex][newPosition.cellIndex])) {
+            collectedLetters.push(map2D[newPosition.rowIndex][newPosition.cellIndex]);
+          }
+          position.rowIndex = newPosition.rowIndex;
+          position.cellIndex = newPosition.cellIndex;
+          visited.add(newPosKey);
+          previousDirection = direction;
+          foundUnvisited = true;
+          break;
+        }
+      }
+      if (!foundUnvisited) {
+        let continueFromVisited = false;
+        for (const direction of possibleDirections) {
+          const newPosition = { ...position };
+          moveInDirection(newPosition, direction);
+          if (isOutOfBounds(map2D, newPosition)) continue;
+          const newPosKey = `${newPosition.rowIndex},${newPosition.cellIndex}`;
+
+          if (visited.has(newPosKey)) {
+            const innerPossibleDirections: Directions[] = ['right', 'left', 'down', 'up'];
+            for (const innerDirection of innerPossibleDirections) {
+              const innerNewPosition = { ...newPosition };
+              if (/[A-Z]/.test(map2D[newPosition.rowIndex][newPosition.cellIndex]) && !visited.has(`${newPosition.rowIndex},${newPosition.cellIndex}`)) {
+                collectedLetters.push(map2D[newPosition.rowIndex][newPosition.cellIndex]);
+              }
+              moveInDirection(innerNewPosition, innerDirection);
+              if (isOutOfBounds(map2D, innerNewPosition)) continue;
+              const innerNewPosKey = `${innerNewPosition.rowIndex},${innerNewPosition.cellIndex}`;
+
+              if (!visited.has(innerNewPosKey) && isValidMove(map2D, newPosition, innerDirection)) {
+                if (/[A-Z]/.test(map2D[newPosition.rowIndex][newPosition.cellIndex]) && !visited.has(`${newPosition.rowIndex},${newPosition.cellIndex}`)) {
+                  collectedLetters.push(map2D[newPosition.rowIndex][newPosition.cellIndex]);
+                }
+                path.push(map2D[newPosition.rowIndex][newPosition.cellIndex]);
+                position.rowIndex = innerNewPosition.rowIndex;
+                position.cellIndex = innerNewPosition.cellIndex;
+                visited.add(innerNewPosKey);
+                previousDirection = innerDirection;
+                continueFromVisited = true;
+                break;
+              }
+            }
+            if (continueFromVisited) break;
+          }
+        }
+        if (!continueFromVisited) break;
+      }
+    } else {
+      path.push(currentChar);
+      visited.add(posKey);
+
+      if (isEndingPosition(position, endingPosition)) {
+        path.push('x');
+        break;
+      }
+
+      if (/[A-Z]/.test(currentChar)) {
+        collectedLetters.push(currentChar);
+      }
+
+      if (currentChar === '+' || /[A-Z]/.test(currentChar)) {
+        if (previousDirection) {
+          const newDirection = updateDirection(map2D, position, previousDirection);
+          if (newDirection) {
+            previousDirection = newDirection;
+          } else {
+            break;
+          }
+        } else {
+          break;
+        }
+      } else {
+        if (previousDirection) {
+          moveInDirection(position, previousDirection);
+        }
+      }
     }
   }
 
-  console.log(path);
+  if (!path.includes('x') && isEndingPosition(position, endingPosition)) {
+    path.push('x');
+  }
 
-  return path;
+  return { path, collectedLetters, error: null };
+};
+
+const determineStartingDirection = (map2D: MapArray, position: Position): Directions | null => {
+  if (position.cellIndex + 1 < map2D[0].length && isValidPathChar(map2D[position.rowIndex][position.cellIndex + 1])) {
+    position.cellIndex++;
+    return 'right';
+  }
+  if (position.cellIndex - 1 >= 0 && isValidPathChar(map2D[position.rowIndex][position.cellIndex - 1])) {
+    position.cellIndex--;
+    return 'left';
+  }
+  if (position.rowIndex + 1 < map2D.length && isValidPathChar(map2D[position.rowIndex + 1][position.cellIndex])) {
+    position.rowIndex++;
+    return 'down';
+  }
+  if (position.rowIndex - 1 >= 0 && isValidPathChar(map2D[position.rowIndex - 1][position.cellIndex])) {
+    position.rowIndex--;
+    return 'up';
+  }
+  return null;
+};
+
+const updateDirection = (map2D: MapArray, position: Position, previousDirection: Directions): Directions | null => {
+  const possibleDirections: Directions[] = [previousDirection, 'right', 'left', 'down', 'up'];
+  for (const direction of possibleDirections) {
+    if (direction !== oppositeDirection(previousDirection) && isValidMove(map2D, position, direction)) {
+      moveInDirection(position, direction);
+      return direction;
+    }
+  }
+  return null;
+};
+
+const isValidPathChar = (char: string): boolean => {
+  return char === '-' || char === '|' || char === '+' || /[A-Z]/.test(char) || char === 'x';
+};
+
+const isValidMove = (map2D: MapArray, position: Position, direction: Directions): boolean => {
+  const newPosition = { ...position };
+  moveInDirection(newPosition, direction);
+  return !isOutOfBounds(map2D, newPosition) && isValidPathChar(map2D[newPosition.rowIndex]?.[newPosition.cellIndex]);
+};
+
+const moveInDirection = (position: Position, direction: Directions) => {
+  if (direction === 'left') position.cellIndex--;
+  if (direction === 'right') position.cellIndex++;
+  if (direction === 'up') position.rowIndex--;
+  if (direction === 'down') position.rowIndex++;
+};
+
+const oppositeDirection = (direction: Directions): Directions => {
+  const opposites: { [key in Directions]: Directions } = {
+    up: 'down',
+    down: 'up',
+    left: 'right',
+    right: 'left',
+  };
+  return opposites[direction];
+};
+
+const isOutOfBounds = (map2D: MapArray, position: Position): boolean => {
+  return (
+    position.rowIndex < 0 ||
+    position.rowIndex >= map2D.length ||
+    position.cellIndex < 0 ||
+    position.cellIndex >= map2D[0].length
+  );
+};
+
+const isEndingPosition = (position: Position, endingPosition: Position): boolean => {
+  return position.rowIndex === endingPosition.rowIndex && position.cellIndex === endingPosition.cellIndex;
 };
